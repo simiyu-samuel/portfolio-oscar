9a58fbf90c9ffd0cf9c28490bcb476c175ce52ed
Simiyu Samuel <98589218+simiyu-samuel@users.noreply.github.com>
2026-03-09 22:04:53 +0300

    Fix JavaScript scroll errors and optimize font loading

--- script.js
+++ script.js
@@ -80,11 +80,14 @@ function scrollActive(){
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
+        const link = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        if(link){
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                link.classList.add('active-link')
+            }else{
+                link.classList.remove('active-link')
+            }
         }
     })
 }
